a647d97e8fa499f65024f7a388998280f3bc2fe7
karino-t <karino-t@klab.com>
2014-07-28 12:26:22 +0900

    add network_api_cc include success

---
+++ network_api_cc/netapi.cpp
@@ -0,0 +1,27 @@
+// neapi.cpp
+//
+// http://numenta.org/docs/nupic.core/
+
+//
+//#define NTA_PLATFORM_linux64
+#define NTA_PLATFORM_darwin64
+
+#include <iostream>
+
+#include <nta/engine/Network.hpp>
+#include <nta/engine/NuPIC.hpp>
+#include <nta/engine/Region.hpp>
+#include <nta/ntypes/Dimensions.hpp>
+
+
+using namespace std;
+using namespace nta;
+
+int main()
+{
+    // inheritance
+    cout << "## inheritance" << endl;
+
+    Network net; 
+}
+
